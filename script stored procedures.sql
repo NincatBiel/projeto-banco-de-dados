adbba6774317f028f8733ee7af6d4d8f48ecde3b
Gabriel_zoom <lgas1679@gmail.com>
2026-09-10 12:31:37 -0300

    Add stored procedure for animal consultation history
    Created a stored procedure to retrieve the consultation history of a specific animal.

--- script stored procedures.sql
+++ script stored procedures.sql
@@ -16,5 +16,26 @@ CALL sp_cadastrar_cliente(/*insira o nome*/, /*insira o telefone*/, /*insira o e
 -- agendar consulta nova
 
 -- historico de um animal especifico
+DELIMITER //
+
+CREATE PROCEDURE historico_animal(IN animal_nome VARCHAR(20))
+BEGIN
+    SELECT
+        a.nome AS 'Animal',
+        c.data_hora AS 'Data da consulta',
+        c.status AS 'Status',
+        d.diagnostico AS 'Diagnóstico'
+    FROM animal a
+    INNER JOIN consulta c
+        ON a.animal_id = c.fk_animal_id
+    LEFT JOIN diagnostico d
+        ON c.consulta_id = d.fk_consulta_id
+    WHERE a.nome = animal_nome
+    ORDER BY c.data_hora DESC;
+END //
+
+DELIMITER ;
+
+CALL historico_animal('');
 
 DELIMITER ;

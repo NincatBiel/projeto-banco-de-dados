ab66fac17ce4a19ae22a0bb7b1f001ebb8b57935
arthur <clein4879@gmail.com>
2026-09-10 17:31:44 -0300

    Create multiple indexes for query optimization

--- script index.sql
+++ script index.sql
@@ -1,4 +1,18 @@
 -- index pra pesquisar ou ordenar consultas pela data
+CREATE INDEX idx_consulta_vet
+ON consulta(data_hora);
 
 CREATE INDEX idx_consulta_data
 ON consulta(data_hora);
+
+CREATE INDEX idx_cliente_nome 
+ON tb_cliente(nome);
+
+CREATE INDEX idx_veterinario_crmv 
+ON tb_veterinario(crmv);
+
+CREATE INDEX idx_animal_nome
+ON tb_animal(nome);
+
+CREATE INDEX idx_medicamento_nome
+ON tb_medicamento(nome);

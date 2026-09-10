df6a15006f9168e35f423998960120acc52e26b2
Gabriel Ramos <gabriel.alphatecnologia@gmail.com>
2026-09-10 11:28:54 -0300

    Stored procedures | sp_cadastro_cliente
    Created stored procedure to register a new client with name, phone, email, and CPF.

--- script stored procedures.sql
+++ script stored procedures.sql
@@ -1,7 +1,21 @@
+DELIMITER //
 -- inserir um cliente novo
+CREATE PROCEDURE sp_cadastrar_cliente
+    @nome VARCHAR(100),
+    @telefone VARCHAR(17),
+    @email VARCHAR(100),
+    @cpf VARCHAR(20)
+    AS
+    BEGIN
+        INSERT INTO tb_cliente (nome, telefone, email, cpf)
+        VALUES (@nome, @telefone, @email, @cpf);
+    END //
 
+EXEC sp_cadastrar_cliente @nome = /*insira o nome*/, @telefone = /*insira o telefone*/, @email = /*insira o email*/, @cpf = /*insira o cpf*/;
 -- inserir um animal novo
 
 -- agendar consulta nova
 
--- se quiser: inserir: serviço, procedimento e medicamento novos (n precisa fazer, os de cima são mais importantes)
+-- historico de um animal especifico
+
+DELIMITER ;

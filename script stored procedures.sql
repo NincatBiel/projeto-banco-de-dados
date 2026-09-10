9326e1923610acdbba9eb552f04a912b2474baaa
Gabriel Ramos <gabriel.alphatecnologia@gmail.com>
2026-09-10 14:13:21 -0300

    fixed stored procedure

--- script stored procedures.sql
+++ script stored procedures.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE sp_cadastrar_cliente(
     IN cpf VARCHAR(20))
     BEGIN
         INSERT INTO tb_cliente (nome, telefone, email, cpf)
-        VALUES (@nome, @telefone, @email, @cpf);
+        VALUES (nome, telefone, email, cpf);
     END //
 
 -- CALL sp_cadastrar_cliente(/*insira o nome*/, /*insira o telefone*/, /*insira o email*/, /*insira o cpf*/);
@@ -16,17 +16,18 @@ CREATE PROCEDURE sp_cadastrar_cliente(
 -- agendar consulta nova
 
 -- historico de um animal especifico
-CREATE PROCEDURE historico_animal(IN animal_nome VARCHAR(20))
+CREATE PROCEDURE sp_historico_animal(
+        IN animal_nome VARCHAR(20))
 BEGIN
     SELECT
         a.nome AS 'Animal',
         c.data_hora AS 'Data da consulta',
         c.status AS 'Status',
         d.diagnostico AS 'Diagnóstico'
-    FROM animal a
-    INNER JOIN consulta c
+    FROM tb_animal a
+    INNER JOIN tb_consulta c
         ON a.animal_id = c.fk_animal_id
-    LEFT JOIN diagnostico d
+    LEFT JOIN tb_diagnostico d
         ON c.consulta_id = d.fk_consulta_id
     WHERE a.nome = animal_nome
     ORDER BY c.data_hora DESC;

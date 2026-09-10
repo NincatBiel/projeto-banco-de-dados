b895292a6fff3457c47d8b16868f5cabd157af72
arthur <clein4879@gmail.com>
2026-09-10 20:01:12 -0300

    Atualizar o script users.sql

--- script users.sql
+++ script users.sql
@@ -8,3 +8,9 @@ GRANT ALL ON clinica_veterinaria.* TO 'administrador'@'localhost';
 CREATE USER 'atendente'@'localhost' IDENTIFIED BY 'atendente123';
 
 GRANT EXECUTE ON PROCEDURE clinica_veterinaria.sp_agendar_consulta TO 'atendente'@'localhost';
+
+-- Veterinário
+
+CREATE USER 'veterinario'@'localhost' IDENTIFIED BY 'vet123';
+
+GRANT EXECUTE ON PROCEDURE clinica_veterinaria.sp_add_consulta TO 'veterinario'@'localhost';
